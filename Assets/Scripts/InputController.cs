c0e3ef38b3f2ac96d793dd1a71a15b64eb67efe2
DenisPanfilov0 <123986275+DenisPanfilov0@users.noreply.github.com>
2023-07-30 15:41:49 +0300

    Character

--- Assets/Scripts/InputController.cs
+++ Assets/Scripts/InputController.cs
@@ -3,8 +3,8 @@ using UnityEngine;
 public class InputController : MonoBehaviour
 {
     [SerializeField] private float _moveSpeed = 4f;
+    [SerializeField] private float _rotationSpeed = 5f; // Adjust this to control camera rotation speed
 
-    private Vector3 forward, right;
     private Transform characterTransform;
     private Transform cameraTransform;
 
@@ -12,24 +12,29 @@ public class InputController : MonoBehaviour
     {
         characterTransform = transform;
         cameraTransform = Camera.main.transform;
-
-        forward = cameraTransform.forward;
-        forward.y = 0;
-        forward = Vector3.Normalize(forward);
-        right = Quaternion.Euler(new Vector3(0, 90, 0)) * forward;
     }
 
     private void Update()
     {
         if (Input.anyKey)
+        {
             Move();
+        }
+        
+        float rotationInput = Input.GetAxis("Mouse ScrollWheel");
+        RotateCamera(rotationInput);
     }
 
     private void Move()
     {
+        Vector3 forwardDirection = cameraTransform.forward;
+        forwardDirection.y = 0;
+        forwardDirection = Vector3.Normalize(forwardDirection);
+        Vector3 rightDirection = Quaternion.Euler(new Vector3(0, 90, 0)) * forwardDirection;
+
         Vector3 direction = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
-        Vector3 rightMovement = right * _moveSpeed * Time.deltaTime * Input.GetAxis("Horizontal");
-        Vector3 upMovement = forward * _moveSpeed * Time.deltaTime * Input.GetAxis("Vertical");
+        Vector3 rightMovement = rightDirection * _moveSpeed * Time.deltaTime * Input.GetAxis("Horizontal");
+        Vector3 upMovement = forwardDirection * _moveSpeed * Time.deltaTime * Input.GetAxis("Vertical");
 
         Vector3 heading = Vector3.Normalize(rightMovement + upMovement);
 
@@ -37,7 +42,17 @@ public class InputController : MonoBehaviour
         characterTransform.position += rightMovement;
         characterTransform.position += upMovement;
 
-        // Update the camera position to center on the character
         cameraTransform.position = characterTransform.position;
     }
+
+    private void RotateCamera(float rotationInput)
+    {
+        if (Mathf.Abs(rotationInput) > 0.0f)
+        {
+            cameraTransform.RotateAround(characterTransform.position, Vector3.up, rotationInput * _rotationSpeed);
+        }
+    }
+  
 }
+
+
