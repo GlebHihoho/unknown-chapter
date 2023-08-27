1e830dc7f1ccae01e8c2545ed70ede8f91ebcf62
GlebHihoho <hihoho@mail.ru>
2023-08-27 21:42:31 +0300

    some refactoring

--- Assets/Scripts/MainCharacteristics.cs
+++ Assets/Scripts/MainCharacteristics.cs
@@ -16,6 +16,32 @@ public class MainCharacteristics : MonoBehaviour
         var ds = GetComponent<DialogueSystemTrigger>();
     }
 
+    private void Update()
+    {
+        var textDescription = DialogueLua.GetQuestField("Захоронение тел", "Description").luaValue;
+
+        if (Input.GetKeyDown(KeyCode.T))
+        {
+            DialogueLua.SetQuestField("Захоронение тел", "Description", textDescription + "/\n111");
+        }
+    }
+
+    private void OnEnable()
+    {
+        // Lua.RegisterFunction("PhysicalAbilities", this, SymbolExtensions.GetMethodInfo(() => _physicalAbilities((int)0)));
+        Lua.RegisterFunction("PhysicalAbilities", this, SymbolExtensions.GetMethodInfo(() => SetPhysicalAbilities(1)));
+        Lua.RegisterFunction("Perception", this, SymbolExtensions.GetMethodInfo(() => SetPerception(1)));
+        Lua.RegisterFunction("Intellect", this, SymbolExtensions.GetMethodInfo(() => SetIntellect(1)));
+
+    }
+
+    private void OnDisable()
+    {
+        Lua.UnregisterFunction("PhysicalAbilities");
+        Lua.UnregisterFunction("Perception");
+        Lua.UnregisterFunction("Intellect");
+    }
+
     public int GetPhysicalAbilities()
     {
         return _physicalAbilities;
@@ -46,17 +72,6 @@ public class MainCharacteristics : MonoBehaviour
         _intellect = value;
     }
 
-    private void Update()
-    {
-        var textDescription = DialogueLua.GetQuestField("Захоронение тел", "Description").luaValue;
-
-        if (Input.GetKeyDown(KeyCode.T))
-        {
-            DialogueLua.SetQuestField("Захоронение тел", "Description", textDescription + "/\n111");
-
-        }
-    }
-
     public void AddCharacteristic(string nameCharacteristic)
     {
         switch (nameCharacteristic)
@@ -71,21 +86,5 @@ public class MainCharacteristics : MonoBehaviour
                 _intellect++;
                 break;
         }
-    }
-
-    private void OnEnable()
-    {
-        // Lua.RegisterFunction("PhysicalAbilities", this, SymbolExtensions.GetMethodInfo(() => _physicalAbilities((int)0)));
-        Lua.RegisterFunction("PhysicalAbilities", this, SymbolExtensions.GetMethodInfo(() => SetPhysicalAbilities(1)));
-        Lua.RegisterFunction("Perception", this, SymbolExtensions.GetMethodInfo(() => SetPerception(1)));
-        Lua.RegisterFunction("Intellect", this, SymbolExtensions.GetMethodInfo(() => SetIntellect(1)));
-
-    }
-
-    private void OnDisable()
-    {
-        Lua.UnregisterFunction("PhysicalAbilities");
-        Lua.UnregisterFunction("Perception");
-        Lua.UnregisterFunction("Intellect");
     }
 }
