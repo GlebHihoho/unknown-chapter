377a5f70430db6088f59ef944563f9df649486e5
DenisPanfilov0 <123986275+DenisPanfilov0@users.noreply.github.com>
2023-09-06 22:34:37 +0300

    Add highlight

---
+++ Assets/Scripts/CharacterInteraction.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+
+public class CharacterInteraction : MonoBehaviour
+{
+    public float interactionDistance = 2.0f; // Задайте желаемое расстояние для взаимодействия.
+
+    private void Update()
+    {
+        // Получите текущую позицию персонажа и объекта для взаимодействия.
+        Vector3 characterPosition = transform.position;
+        Vector3 interactableObjectPosition = GetInteractableObjectPosition(); // Реализуйте этот метод для получения позиции объекта для взаимодействия.
+
+        // Вычислите расстояние между ними.
+        float distance = Vector3.Distance(characterPosition, interactableObjectPosition);
+
+        // Если расстояние меньше заданного, выполните взаимодействие.
+        if (distance <= interactionDistance)
+        {
+            PerformInteraction(); // Реализуйте этот метод для выполнения взаимодействия.
+        }
+    }
+
+    private Vector3 GetInteractableObjectPosition()
+    {
+        // Реализуйте этот метод для получения позиции объекта для взаимодействия.
+        // Например, вы можете использовать Raycast или другие методы, чтобы найти объект в определенном направлении от персонажа.
+        // Возвращайте позицию найденного объекта.
+        return Vector3.zero;
+    }
+
+    private void PerformInteraction()
+    {
+        // Реализуйте этот метод для выполнения взаимодействия с объектом.
+        // Например, вы можете активировать объект, показать диалоговое окно и т. д.
+    }
+}
---
+++ Assets/Scripts/Hightlight.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using Unity.VisualScripting;
+using UnityEngine;
+
+namespace DefaultNamespace
+{
+    [RequireComponent(typeof(Outline))]
+    public class Hightlight : MonoBehaviour
+    {
+        
+        [SerializeField] private Outline _outline;
+
+        private void Start()
+        {
+            _outline = GetComponent<Outline>();
+        }
+
+        private void Update()
+        {
+            if (Input.GetKey(KeyCode.LeftAlt))
+            {
+                _outline.enabled = true;
+            }
+            else if (Input.GetKeyUp(KeyCode.LeftAlt))
+            {
+                _outline.enabled = false;
+            }
+            
+        }
+        
+        private void OnMouseEnter()
+        {
+            _outline.enabled = true;
+        }
+
+        private void OnMouseExit()
+        {
+            _outline.enabled = false;
+        }
+        
+    }
+}
