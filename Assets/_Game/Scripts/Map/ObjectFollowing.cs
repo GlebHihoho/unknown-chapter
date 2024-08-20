f45da5b35966ee6f168738b26de87f0dc9416dca
Olga Panisova <shadenoff@gmail.com>
2024-08-20 05:45:30 +0300

    Changed smoothing logic for the player's map mark.

--- Assets/_Game/Scripts/Map/ObjectFollowing.cs
+++ Assets/_Game/Scripts/Map/ObjectFollowing.cs
@@ -1,4 +1,3 @@
-using DG.Tweening;
 using UnityEngine;
 
 public class ObjectFollowing : MonoBehaviour
@@ -7,17 +6,24 @@ public class ObjectFollowing : MonoBehaviour
 
     [SerializeField] bool rotate = false;
 
+    [SerializeField] float smoothTime = 0.2f;
+    [SerializeField] float smoothRotateTime = 0.1f;
+
+    Vector3 velocity = Vector3.zero;
+    Vector3 rotateVelocity = Vector3.zero;
+
 
     // Update is called once per frame
     void Update()
     {
+
         Vector3 newPos = new Vector3(target.position.x, transform.position.y, target.position.z);
-        transform.DOMove(newPos, 0.1f);
+        transform.position = Vector3.SmoothDamp(transform.position, newPos, ref velocity, smoothTime);
 
         if (rotate)
         {
             Vector3 newRotation = new Vector3(90, target.rotation.eulerAngles.y, 0);
-            transform.DORotate(newRotation, 0.5f);
+            transform.rotation = Quaternion.Euler(Vector3.SmoothDamp(transform.rotation.eulerAngles, newRotation, ref rotateVelocity, smoothRotateTime));
         }
     }
 }
