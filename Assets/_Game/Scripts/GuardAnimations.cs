90d020f4ccc9f9703331ce8be942eeb57abfc461
Olga Panisova <shadenoff@gmail.com>
2024-12-09 22:05:05 +0300

    Added animation for the guards rushing to help Foma.

--- Assets/_Game/Scripts/GuardAnimations.cs
+++ Assets/_Game/Scripts/GuardAnimations.cs
@@ -1,18 +1,50 @@
 using UnityEngine;
+using UnityEngine.AI;
 
 public class GuardAnimations : MonoBehaviour
 {
 
+    enum Status { Idle, Running, Helping }
+    Status status = Status.Idle;
+
     Animator animator;
+
+    NavMeshAgent agent;
 
     [SerializeField, Range(0, 100)] int changeThreshold = 80;
     [SerializeField, Min(1)] int idleAnimations = 4;
 
-    private void Awake() => animator = GetComponent<Animator>();
+    [SerializeField] Transform foma;
+
+    private void Awake()
+    {
+        animator = GetComponent<Animator>();
+        agent = GetComponent<NavMeshAgent>();
+    }
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start() => InvokeRepeating("ChangeIdle", 0, 1);
-    
+    void Start() => InvokeRepeating(nameof(ChangeIdle), 0, 1);
+
+
+    private void Update()
+    {
+        if (status == Status.Helping) return;
+
+        if (status == Status.Idle && agent.remainingDistance > 0)
+        {
+            status = Status.Running;
+        }
+
+        if (status == Status.Running && agent.remainingDistance <= 0.1f)
+        {
+            status = Status.Helping;
+
+            transform.LookAt(foma.position);
+            animator.SetTrigger("Run");
+            CancelInvoke(nameof(ChangeIdle));
+        }
+    }
+
 
     private void ChangeIdle()
     {
