9cacefaed1601e009d3f4dd253f81b81d6b87b5e
Olga Panisova <shadenoff@gmail.com>
2025-01-02 11:03:44 +0300

    Added confirmation before exiting the game.

--- Assets/Scripts/UI/ExitButton.cs
+++ Assets/Scripts/UI/ExitButton.cs
@@ -4,9 +4,16 @@ namespace UI
 {
     public class ExitButton : MonoBehaviour
     {
+        [SerializeField] ModalWindow modalWindow;
+
         public void ExitGame()
         {
-            Application.Quit();
+            modalWindow.ShowPromt("", "Выйти из игры?", ExitGame);           
+        }
+
+        private void ExitGame(bool confirmed)
+        {
+            if (confirmed) Application.Quit();
         }
     }
 }
