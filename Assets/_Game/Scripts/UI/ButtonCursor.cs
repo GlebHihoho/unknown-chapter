386644070be4ffee42ae08d76ab9e6a627b1b3d4
Olga Panisova <shadenoff@gmail.com>
2025-01-09 11:24:27 +0300

    Cursor now change status when hovering over UI elements (buttons etc.) Added more "click" sounds for elements like buttons in dialogues.

--- Assets/_Game/Scripts/UI/ButtonCursor.cs
+++ Assets/_Game/Scripts/UI/ButtonCursor.cs
@@ -8,7 +8,10 @@ public class ButtonCursor : MonoBehaviour, IPointerEnterHandler, IPointerExitHan
 
     Button button;
 
+    [SerializeField] CursorData interactiveCursor;
     [SerializeField] CursorData unavaialableCursor;
+
+    bool isPointerOver = false;
 
     private void Awake() => button = GetComponent<Button>();
 
@@ -16,11 +19,26 @@ public class ButtonCursor : MonoBehaviour, IPointerEnterHandler, IPointerExitHan
 
     public void OnPointerEnter(PointerEventData eventData)
     {
-        if (!button.interactable) GameCursor.instance.SetCursor(unavaialableCursor);
+        if (button.interactable) GameCursor.instance.SetCursor(interactiveCursor);
+        else GameCursor.instance.SetCursor(unavaialableCursor);
+
+        isPointerOver = true;
     }
 
     public void OnPointerExit(PointerEventData eventData)
     {
-        if (!button.interactable) GameCursor.instance.RestorePreviousCursor();
+        GameCursor.instance.SetCursor(null);
+
+        isPointerOver = false;
+    }
+
+
+    private void OnDisable()
+    {
+        if (isPointerOver) 
+        { 
+            GameCursor.instance.SetCursor(null); 
+            isPointerOver= false;
+        }
     }
 }
