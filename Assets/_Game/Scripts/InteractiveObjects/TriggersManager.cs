70251760c39675f55b21536c39401332849042d3
Olga Panisova <shadenoff@gmail.com>
2025-01-31 06:45:55 +0300

    Fixed an issue where Pivs' trigger for the first meeting was not saved.

--- Assets/_Game/Scripts/InteractiveObjects/TriggersManager.cs
+++ Assets/_Game/Scripts/InteractiveObjects/TriggersManager.cs
@@ -8,21 +8,21 @@ public class TriggersManager : MonoBehaviour, ISaveable
 
     public void Save(ref SaveData.Save save)
     {
-        for (int i = 0; i < triggers.Length; i++)
-        {
-            if (i < save.levels[save.level].triggers.Count)
-                triggers[i].gameObject.SetActive(save.levels[save.level].triggers[i]);
-            else triggers[i].gameObject.SetActive(true);
-        }
-    }
-
-    public void Load(SaveData.Save save)
-    {
         save.levels[save.level].triggers.Clear();
 
         for (int i = 0; i < triggers.Length; i++)
         {
             save.levels[save.level].triggers.Add(triggers[i].gameObject.activeSelf);
+        }
+    }
+
+    public void Load(SaveData.Save save)
+    {
+        for (int i = 0; i < triggers.Length; i++)
+        {
+            if (i < save.levels[save.level].triggers.Count)
+                triggers[i].gameObject.SetActive(save.levels[save.level].triggers[i]);
+            else triggers[i].gameObject.SetActive(true);
         }
     }
 }
